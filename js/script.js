0363cd1042ff3215835f603d15019fdcafecd87c
justin-hill426 <jrhill426@gmail.com>
2021-05-04 11:29:52 -0700

    Added functionality to PrintQuotes and Pick a Random quote

--- js/script.js
+++ js/script.js
@@ -3,13 +3,23 @@ Treehouse FSJS Techdegree:
 project 1 - A Random Quote Generator
 ******************************************/
 
-// For assistance: 
+// For assistance:
   // Check the "Project Resources" section of the project instructions
   // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
 
-/*** 
- * `quotes` array 
+/***
+ * `quotes` array
 ***/
+
+const quotes = [
+  {
+    quote : "Simplicity is the Ultimate Sophistication",
+    source : "Leonardo Da Vinci",
+  },
+
+
+
+]
 
 
 
@@ -17,11 +27,23 @@ project 1 - A Random Quote Generator
  * `getRandomQuote` function
 ***/
 
+const getRandomQuote = quotes => {
+  const randomNumber = Math.floor(Math.random() * quotes.length);
+  return quotes[randomNumber];
+};
 
 
 /***
  * `printQuote` function
 ***/
+
+const printQuote = () => {
+  let html = "";
+  const quote = getRandomQuote(quotes);
+  html += `<p class = "quote">${quote.quote}</p>`;
+  html += `<p class = "source">${quote.source}</p>`;
+  document.getElementById('quote-box').innerHTML = html;
+};
 
 
 
